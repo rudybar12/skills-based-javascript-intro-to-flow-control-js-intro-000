e95ae4f4f92a609f5642aa072b808e48c0d7f3cb
rodolfo Barrientos <rudybar12@yahoo.com>
2020-06-15 06:24:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,26 @@
 function basicTeenager(age) {
-
+if (age == 13 || 14 || 15 || 16 || 17 || 18 || 19 ) {
+  console.log('You are a teenager!')
+  }
 }
 
 function teenager(age) {
-
+if (age >=13 && <=19){
+  console.log('You are a teenager!')
+} else {
+  console.log('You are not a teenager!')
+  }
 }
 
 function ageChecker(age) {
-
+if (age >=13 && <=19) {
+  console.log('You are a teenager!'){
+  }else if (age >12) {
+    console.log('You are a kid'){
+    } esle (age <19)
+    console.log('You are a grownup')
+  }
+  }
 }
 
 function ternaryTeenager(age) {
