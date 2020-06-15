646b796b20b1c4cf22a64e0a773b6c90e9249cb6
rodolfo Barrientos <rudybar12@yahoo.com>
2020-06-15 07:24:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -40,19 +40,19 @@ break;
 break;
     case 15:
     return "You are a teeneger";
-      break;
-      case 16:
-        return "You are a teenager";
-        break;
-      case 17:
-        return "You are a teenager";
-        break;
+break;
+    case 16:
+    return "You are a teenager";
+break;
+    case 17:
+    return "You are a teenager";
+break;
     case 18:
     return "You are a teenager";
-      break;
+break;
     case 19:
-      return "You are a teenager";
-      break;
+    return "You are a teenager";
+    break;
 
     default:
       return "You are have an age";
